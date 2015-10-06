f76af2eec197c6d49e7eb109dca66217db18bf61
binny master <binnymaster@gmail.com>
2015-10-06 12:05:03 -0400

    modifed productpage

--- public/javascripts/productpage.js
+++ public/javascripts/productpage.js
@@ -1,11 +1,38 @@
 
-   $(document).ready(function(){
+   $(document).ready(function(e){
             var txt = $("input");
             var additem= $("#additem");
             var description=$("#des");
             var price=$("#price");
             var add =$("#add");        
             var i = {};
+            $.ajax({
+                    url : "/users/",
+                    type : "get",
+                    data : i,
+                    success :function(name){
+                    console.log("hi");
+                    console.log(name);
+                    $.each( name, function(Id,additem ,description,price)
+                    {
+                    console.log(name);
+                    var c1=$('<div />', { class:'well  well-lg',value: $(this).attr('id'),name:'div2',height:'55px'});
+                    $('#ad').append(c1);
+                    var c6 =$('<div />', { class:'col-md-8',name:'innerdiv1'});
+                    var c7 =$('<div />', { class:'row form-inline col-md-4',name:'innerdiv1'})
+                    $(c1).append(c6,c7);
+                    var c2 =$('<h3/>',{text:$(this).attr("additem")});
+                    var c8=$('<br>');
+                    var c4 =$('<button />', {type:'button',class:'btn btn-primary col-xs-2',text: $(this).attr('price')});
+                    var c5=$('<h4/>',{class:'glyphicon glyphicon-remove-sign pull-right',id:$(this).attr('id')});
+                    var c3 =$('<p/>',{text:$(this).attr("description")});
+                     $(c6).append(c2,c3);
+                     $(c7).append(c8,c4,c5);
+                    $(c5).on('click',spa);   
+                })
+               }
+            })
+
             $("#add").on("click",function(e){
               e.preventDefault()
                 $("input").each(function(j){
@@ -13,73 +40,44 @@
                       $(txt[j]).val();
                      console.log($(txt[j]).val());  
                  });
-
-                
-               $.ajax({
-                    url : "/users",
-                    type : "post",
-                    
-                    data : i,
-                    success :function(name){
-                    console.log("hi");
-                    console.log(name);
-                   
-                    } 
-            })
             $.ajax({
-             url : "/users/",
-
-              type : "get",
-              //type : "delete",      
+             url : "/users",
+              type : "post",      
               data : i,
               success :function(data){
-              
-              console.log(data);   
-               
-               $.each( data, function(Id,additem ,description,price,event) {
-               
-                     
-                    var c1=$('<div />', { class:'well  well-lg',value: $(this).attr('id'),name:'div2',height:'50px'});
+              console.log(data.id);    
+                    var c1=$('<div />', { class:'well  well-lg',value: data.id,name:'div2',height:'50px'});
                     $('#ad').append(c1);
-                    var c2 =$('<h4/>',{class:'col-xs-7',text:$(this).attr("additem")});
-                    
-                    var c4 =$('<button />', { type:'button',class:'btn btn-primary col-xs-2',text: $(this).attr('price')});
-                    var c5 =$('<span/>',{class:'glyphicon glyphicon-remove-sign col-xs-3 ', id: $(this).attr('id') });
-                    var c3 =$('<p/>',{class:'col-xs-12 ',text:$(this).attr("description")});
-                     $(c1).append(c2,c4,c5,c3);
-
-                      
-
-
-                    
-                    $(c5).click(function(){
-                        console.log(c5.attr('id'));
-                      for(var j=0; j<c1.length; j++)
-                            {
-                                console.log($(c1[j]).text());
-                                if($(this).attr('id')=== $(this).attr('id')) 
-                                {
-                                    console.log("hi");
-                                     $(c1[j]).remove();
+                    var c6 =$('<div />', { class:'col-md-8',name:'innerdiv1'});
+                    var c7 =$('<div />', { class:'col-md-4',name:'innerdiv1'})
+                    $(c1).append(c6,c7);
+                    var c2 =$('<h4/>',{class:'col-xs-7',text:data.additem});
+                    var c8=$('<br>');
+                    var c4 =$('<button />', { type:'button',class:'btn btn-primary col-xs-2',text:data.price });
+                    var c5=$('<h4/>',{class:'glyphicon glyphicon-remove-sign pull-right',id:data.id});
+                    //var c5 =$('<button/>',{class:' btn btn-link-nounderline glyphicon glyphicon-remove-sign col-xs-1 pull-right',id:data.id,width:'auto',name:'delete' });
+                    var c3 =$('<p/>',{class:'col-xs-12 ',text:data.description});
+                    $(c6).append(c2,c3);
+                     $(c7).append(c8,c4 ,c5);
+                     $(c5).on('click',spa);
+                }
+            }) })
+                      var spa=  function(){
+                        console.log('bye');
+                        console.log($(this).attr('id'));
+                   
+                                      var spa = $(this).closest('div.well-lg');
                                      $.ajax({
-                                     url : '/users/'+ $(c5).attr('id'),
+                                     url : '/users/'+ $(this).attr('id'),
                                         type : "DELETE",
-                                        data : {"id":$(c5).attr('id')},
+                                        data : {"id":$(this).attr('id')},
                                             success : function(){
                                                 console.log($(this));
+                                                spa.remove();
                                                              
                                              }
                                          })
-                                     
-                                }
-                            }
-                     });
-
-
-                     });
-   
-            }
-            })
-             })
+                             }
+             
         });
  
