55f9e0f4029afb43411b56e8f4c804fa0bee75bf
binny master <binnymaster@gmail.com>
2015-10-11 21:42:53 -0400

    angular service

---
+++ public/javascripts/angularservice.js
@@ -0,0 +1,32 @@
+var app = angular.module('app', []);
+
+app.service('MathService', function() {
+    this.add = function(a, b) { return a + b };
+    
+    this.subtract = function(a, b) { return a - b };
+    
+    this.multiply = function(a, b) { return a * b };
+    
+    this.divide = function(a, b) { return a / b };
+});
+
+app.service('CalculatorService', function(MathService){
+    
+    this.square = function(a) { return MathService.multiply(a,a); };
+    this.cube = function(a) { return MathService.multiply(a, MathService.multiply(a,a)); };
+   this.add=function(a,b){return MathService.add(a,b);};
+});
+
+app.controller('CalculatorController', function($scope, CalculatorService) {
+
+    $scope.doSquare = function() {
+        $scope.answer = CalculatorService.square($scope.number);
+    }
+
+    $scope.doCube = function() {
+        $scope.answer = CalculatorService.cube($scope.number);
+    }
+    $scope.add = function() {
+        $scope.answer = CalculatorService.add($scope.number,$scope.number1);
+    }
+});
